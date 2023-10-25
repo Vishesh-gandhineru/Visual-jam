c2048533ef9943cae33c239915dcf51b6e7a85c2
Vishesh jhadi <visheshjhadi29@gmail.com>
2023-10-25 15:33:16 +0530

    updated the service section home page

--- src/Components/Home components/Services.jsx
+++ src/Components/Home components/Services.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
 import Iconbox from "../Iconbox";
 
 export default function Services() {
   return (
     <section className="OurService my-10">
       <div className=" relative flex flex-col md:flex-row justify-between">
-        <h2 className=" sticky top-0 text-mob-h2 md:text-[60px] uppercase font-semibold text-center md:text-left tracking-wider">
+        <div  className="flex flex-col justify-between">
+        <h2 className=" sticky top-10 mb-[250px]  text-mob-h2 md:text-[60px] uppercase font-semibold text-center md:text-left tracking-wider">
           OUR SERVICES
         </h2>
+        <div className="meetOur flex flex-col md:flex-row justify-center items-center">
+          <img src="/public/meetTeam.svg" className="w-[40%] m-auto"  />
+         <Link to="our-team" className="bg-[#0099ff] text-[#fff] p-3 rounded uppercase font-bold"> meet our crew</Link>
+        </div>
+        </div>
         <div className="serviceSection my-5 w-full md:w-[50%] flex flex-col gap-5">
           <Iconbox
             icon="/public/Custom Illustration.png"
