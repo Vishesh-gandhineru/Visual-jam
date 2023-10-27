f58b95fe90be0fbbeba8a20aac5bec1827d79fa0
Vishesh jhadi <visheshjhadi29@gmail.com>
2023-10-27 14:03:06 +0530

    fix: fixed the contact form message sending from react to WP

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,37 +2,49 @@ import axios from "axios";
 import { useState } from "react";
 
 export default function Contact() {
-
+  const [formSent, setFormSent] = useState(true);
+  const [showMessage, setShowMessage] = useState(false);
 
   const [formData, setFormData] = useState({
-    'full_name': '',
-    'email': '',
-    'message': ''
+    name: "",
+    email: "",
+    message: "",
   });
- 
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Basic " + btoa("visheshjhadi:hVls 3wu5 fC8b E1q6 QPH5 Oo8v"),
+      },
+    };
+
     try {
       const response = await axios.post(
-        'https://react.visualjam.in/wp-json/jet-cct/contact_form_db',
-        formData
+        "https://react.visualjam.in/wp-json/custom/v1/contact/",
+        formData,
+        config
       );
-  
-      if (response.data.status === 'mail_sent') {
-        alert('Message sent successfully!');
+      console.log(response.data);
+      if (response.data.success === true) {
+        setFormSent(true);
       } else {
-        alert('Message could not be sent.');
+        setFormData(false);
       }
+
+      setShowMessage(true);
     } catch (error) {
-      console.error('There was an error sending the message.', error);
+      console.error("There was an error sending the message.", error);
     }
   };
-  
+
   const handleChange = (event) => {
     setFormData({
       ...formData,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
@@ -40,20 +52,66 @@ export default function Contact() {
     <section className="contactUs">
       <div className="my-10 flex justify-between ">
         <div className="flex flex-col justify-between gap-5 w-[40%]">
-          <h1 className="font-semibold text-mob-h1 md:text-[80px] md:mt-[30px] uppercase">GET IN TOUCH</h1>
+          <h1 className="font-semibold text-mob-h1 md:text-[80px] md:mt-[30px] uppercase">
+            GET IN TOUCH
+          </h1>
           <h4 className="text-mob-h5 md:text-desk-h5">
             OFFICE HOURS
-            <span className="block text-mob-p text-[#666666]"> MON - FRI : 10AM TO 6PM IST</span>{" "}
+            <span className="block text-mob-p text-[#666666]">
+              {" "}
+              MON - FRI : 10AM TO 6PM IST
+            </span>{" "}
           </h4>
         </div>
 
-        <div className="ContactForm my-10 w-1/2" >
-          <form className="flex flex-col gap-4 items-center md:gap-8" onSubmit={handleSubmit}>
-            <input type="text" name="full_name" id="your-name" placeholder="Your Name" value={formData['your-name']} onChange={handleChange} className="bg-[#f8f8f8] w-full p-4 placeholder:text-[#666] font-semibold" />
-            <input type="email" name="email" id="your-email" placeholder="Your Email" value={formData['your-email']} onChange={handleChange} className="bg-[#f8f8f8] w-full p-4 placeholder:text-[#666] font-semibold" />
-            <textarea name="message" id="your-message" placeholder="Your Message" value={formData['your-message']} onChange={handleChange} className="bg-[#f8f8f8] w-full p-4 placeholder:text-[#666] font-semibold" />
-            <button type="submit" className="bg-[black] text-[white] p-4 w-full font-semibold">Send Message</button>
+        <div className="ContactForm my-10 w-1/2">
+          <form
+            className="flex flex-col gap-4 items-center md:gap-8"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              name="name"
+              id="your-name"
+              placeholder="Your Name"
+              value={formData["your-name"]}
+              onChange={handleChange}
+              className="bg-[#f8f8f8] w-full p-4 placeholder:text-[#666] font-semibold"
+            />
+            <input
+              type="email"
+              name="email"
+              id="your-email"
+              placeholder="Your Email"
+              value={formData["your-email"]}
+              onChange={handleChange}
+              className="bg-[#f8f8f8] w-full p-4 placeholder:text-[#666] font-semibold"
+            />
+            <textarea
+              name="message"
+              id="your-message"
+              placeholder="Your Message"
+              value={formData["your-message"]}
+              onChange={handleChange}
+              className="bg-[#f8f8f8] w-full p-4 placeholder:text-[#666] font-semibold"
+            />
+            <button
+              type="submit"
+              className="bg-[black] text-[white] p-4 w-full font-semibold"
+            >
+              Send Message
+            </button>
           </form>
+          {showMessage &&
+            (formSent ? (
+              <div className="p-5 text-[#270] text-[16px] capitalize bg-[#DFF2BF] text-center font-semibold">
+                form successfully submitted
+              </div>
+            ) : (
+              <div className="p-5 text-[#D8000C] text-[16px] capitalize bg-[#FFBABA] text-center font-semibold">
+                Error submitting
+              </div>
+            ))}
         </div>
       </div>
       <div className="bg-color-blue md:mx-[-36px] mx-[-23px] text-[white] p-7 flex justify-between items-center">
